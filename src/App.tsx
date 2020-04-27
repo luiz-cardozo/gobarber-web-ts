835cf6927325af54232f321325de4a3293ed13b2
Luiz Fernando Cardozo <luizfcardozo@gmail.com>
2020-04-27 17:00:32 -0300

    add api service  and context to sigin

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import GlobalStyle from './styles/global'
 import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
+
+import { AuthProvider } from './hooks/AuthContext';
+import ToastContainer from './components/ToasContainer';
 
 const App: React.FC = () => 
 <>
-<SignIn />
-<GlobalStyle /> 
+  <AuthProvider>
+    <SignIn />
+    <ToastContainer />
+  </AuthProvider>
+  <GlobalStyle /> 
 </>
 export default App;
